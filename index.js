8991046ca0f0590a026d4fc0bbf72ea327114be8
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-01-12 18:12:35 -0300

    Add ContaCorrente

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    agencia;
+    saldo;    
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-const cliente3nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente(); 
 
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2);
