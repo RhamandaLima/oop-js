b4f8a5fa35abe2727f5f7d87ed54ba1fcb3e69de
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-01-11 21:05:14 -0300

    Preenchimento da classe com atributos e novo cliente

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 class Cliente {
-    
+    nome;
+    cpf;
+    agencia;
+    saldo;
 }
+
+new Cliente();
 
 const cliente1Nome = "Ricardo";
 const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001; 
-const cliente1Saldo = 0; 
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
 
 const cliente2nome = "Alice";
 const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001; 
-const cliente2Saldo = 0; 
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
 
 const cliente3nome = "Alice";
 const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001; 
-const cliente3Saldo = 0; 
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
 console.log(cliente1Nome);
