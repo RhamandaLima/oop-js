75dfaebda52a3d5d79eaa89b46074c81ddbc17d0
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-01-11 21:15:06 -0300

    Removendo um atributo de uma instância (ex: cliente) de uma classe

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ class Cliente {
     cpf;
     agencia;
     saldo;
-    rg;
 }
 
 const cliente1 = new Cliente();
@@ -13,13 +12,11 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 cliente2.agencia = 1001;
 cliente2.saldo = 0;
-cliente2.rg = 987654321;
 
 const cliente3nome = "Alice";
 const cliente3CPF = 88822233309;
