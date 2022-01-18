e0f0b74b4cedd61be97ef94dbbdadbb1fc05a23e
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-01-18 17:23:27 -0300

    Demonstração de undefined no cliente

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+// conta2.cliente = new Cliente();
+//conta2.cliente.nome = "Alice";
+//conta2.cliente.cpf = 88822233309;
 conta2.agencia = 102;
 
 let valor = 200; 
 contaCorrenteRicardo.transferir(valor, conta2);
-
-console.log("Valor: ", valor);
 console.log(conta2);
