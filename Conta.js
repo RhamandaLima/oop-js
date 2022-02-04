1bd9b7e3ab010043794535875f13aa60458d2323
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-03 21:15:11 -0300

    Add alterações de herança em sacar

--- Conta.js
+++ Conta.js
@@ -1,15 +1,43 @@
 export class Conta {
-    constructor(saldoInicial, cliente, agencia){
+    constructor(tipo, saldoInicial, cliente, agencia){
+        this._tipo = tipo;
         this._saldo = saldoInicial;
         this._cliente = cliente; 
         this._agencia = agencia;
     }
 
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente; 
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
     sacar(valor) {
-        taxa = 1.1 * valor; 
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
+        let = taxa = 1;
+        if(this._tipo == "corrente"){
+            taxa = 1.1;
+        }
+
+        if(this._tipo == "salario"){
+            taxa = 1.05;
+        }
+
+        if(this._tipo == "empresarial"){
+            taxa = 1.15;
+        }
+
+        const valorSacado = taxa * valor; 
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
     }
 
