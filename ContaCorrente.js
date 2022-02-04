75494caba1bb83f75d6d6eff201eb0aad5ae1a01
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-03 21:29:59 -0300

    Add herança da Conta

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -7,6 +7,7 @@ export class ContaCorrente extends Conta{
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
 
     constructor(cliente, agencia) {
+        super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
 
