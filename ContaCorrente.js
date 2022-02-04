30cef74a6977231a8bc10c675f8e47ee55d046b0
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-03 21:23:49 -0300

    Add herdando da classe Conta

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -7,30 +7,7 @@ export class ContaCorrente extends Conta{
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
 
     constructor(cliente, agencia) {
-        this.cliente = cliente;
-        this.agencia = agencia;
-        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }
 }
