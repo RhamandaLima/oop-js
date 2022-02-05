f276e5141f744ab3e6dd870272d9a91d36b1d448
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-05 00:07:27 -0300

    Add get senha

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionario/Gerente.js";
 import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 const gerente = new Gerente("Ricardo", 5000, 12378945601);
 
-SistemaAutenticacao.login(diretor, "123456789");
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+
+console.log(estaLogado);
