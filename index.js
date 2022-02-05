fbdad9ebc00f17db4fa3c4ed5465057050b69b81
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-05 00:10:30 -0300

    Finalização da aula de polimorfismo

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import { Diretor } from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
 
 console.log(estaLogado);
