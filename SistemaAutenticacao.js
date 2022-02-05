50a54d8f2e876f66f253380f78d970bb2224c799
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-02-05 01:24:47 -0300

    Add Ducky Type

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,16 +1,18 @@
 /* 
 Ser autenticavel significa ter o método autenticar.
+
+Ducky type: o que interesse é se ele tem o método da propriedade que eu preciso utilizar.
 */
 
-export class SistemaAutenticacao{
-    static login(autenticavel, senha){
-        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+export class SistemaAutenticacao {
+    static login(autenticavel, senha) {
+        if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
             return autenticavel.autenticar(senha);
         }
         return false;
     }
 
-    static ehAutenticavel(autenticavel){
+    static ehAutenticavel(autenticavel) {
         return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
